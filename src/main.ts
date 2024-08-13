b8b84287d2e04129ba3f022d844a06a8a2c78416
Lalit Singh <aerolalit@gmail.com>
2024-08-13 16:29:29 +0200

    update port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AllExceptionsFilter } from './modules/cron/AllException.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(30001); // Start the server on port 30001
+  await app.listen(3001); // Start the server on port 30001
 }
 bootstrap();
